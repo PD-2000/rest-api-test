c189d5b0577ebba81cedbfdd28ab94e4782c6054
Pawel Domagala <domagalapawel11@gmail.com>
2023-07-07 22:28:00 +0200

    Add MongoDB & Mongoose

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,72 +1,12 @@
 const express = require('express');
-const shortid = require('shortid');
-const db = require('../db');
 const router = express.Router();
+const SeatController = require('../controllers/seats.controller');
 
-router.get('/', (req, res) => {
-  res.json(db.seats);
-});
-router.get('/random', (req, res) => {
-  const randomIndex = Math.floor(Math.random() * db.seats.length);
-  const randomSeats = db.seats[randomIndex];
-  res.json(randomSeats);
-});
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  const seat = db.seats.find((item) => item.id === id);
-  if(!seat)
-    return res.status(404).json({message: 'Seat not found'});
-
-  res.json(seat);
-});
-router.post('/', (req, res) => {
-  const {day, seat, client, email} = req.body;
-  const hasRequiredFields = day && seat && client && email;
-  if(!hasRequiredFields)
-    return res.status(400).json({message: 'Missing required fields in request body'});
-
-  const isSeatTaken = db.seats.some((item) => item.seat === seat && item.day === day);
-  if(isSeatTaken)
-    return res.status(409).json({message: 'The slot is already taken...'});
-
-  const newSeat = {
-    id: shortid(),
-    day,
-    seat,
-    client,
-    email
-  };
-
-  db.seats.push(newSeat);
-  req.io.emit('seatsUpdated', db.seats);
-  res.status(201).json({message: 'OK'});
-});
-router.put('/:id', (req, res) => {
-  const id = req.params.id;
-  const {day, seat, client, email} = req.body;
-  const pickedSeat = db.seats.find((item) => item.id === id);
-  if(!pickedSeat)
-    return res.status(404).json({message: 'Seat not found'});
-
-  const hasRequiredFields = day && seat && client && email;
-  if(!hasRequiredFields)
-    return res.status(400).json({message: 'Missing required fields in request body'});
-
-  pickedSeat.day = day;
-  pickedSeat.seat = seat;
-  pickedSeat.client = client;
-  pickedSeat.email = email;
-
-  res.json({message: 'OK'});
-});
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  const seatIndex = db.seats.findIndex((item) => item.id === id);
-  if(seatIndex === -1)
-    return res.status(404).json({message: 'Seat not found'});
-
-  db.seats.splice(seatIndex, 1);
-  res.json({message: 'OK'});
-});
+router.get('/seats', SeatController.getAll);
+router.get('/seats/random', SeatController.getRandom);
+router.get('/seats/:id', SeatController.getById);
+router.post('/seats', SeatController.addNew);
+router.put('/seats/:id', SeatController.update);
+router.delete('/seats/:id', SeatController.delete);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const socket = require('socket.io');
+const mongoose = require('mongoose');
 const app = express();
+
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
@@ -10,15 +12,18 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   req.io = io;
   next();
 });
-app.use(cors());
 
-app.use('/api/testimonials', testimonialsRoutes);
-app.use('/api/concerts', concertsRoutes);
-app.use('/api/seats', seatsRoutes);
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+
+mongoose.connect('mongodb://localhost:27017/music_festival', {useNewUrlParser: true, useFindAndModify: false});
+const db = mongoose.connection;
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
@@ -34,3 +39,8 @@ io.on('connection', (socket) => {
 app.use((req, res) => {
   res.status(404).json({message: '404 not found...'});
 });
+
+db.once('open', () => {
+  console.log('Connected to the database');
+});
+db.on('error', (err) => console.log('Error ' + err));
